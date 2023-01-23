f9c8f00eba43f409973ea11a7246cfa1bc4d5c7b
Diana <maritimdiane96@gmail.com>
2023-01-23 19:56:24 +0300

    completed assignment

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
 
-function Article({title,date ="December 15,2020",preview}){
-    return(
+
+const Article = ({ title, date, preview }) => {
+    return (
         <article>
-            <h3>{title}</h3>
-            <small>{date}</small>
-            <p>{preview}</p>
+            <h3> {title} </h3>
+            <small>{date ? date : 'January 1, 1970'}</small>
+            <p> {preview} </p>
         </article>
     )
-}
+};
 
 export default Article;
